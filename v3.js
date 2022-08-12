5748765af3b40dde507fcb42a3103dbe4b7aa2d9
xphilpj <49448752+xphilpj@users.noreply.github.com>
2022-08-13 01:42:05 +0200

    Improved a lot of things & design refinements
    Cookies currently disabled

--- v3.js
+++ v3.js
@@ -1,10 +1,26 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 function defaultValues(){
     let listCities = ''
     let listTimes = ''
     // Cookie erstellen
     if(document.cookie.length == 0){
         console.log('No cookie found!')
-        document.cookie = 'Braunschweig Hbf\nWarsaw Centralna\nWien;2022-09-19T06:00:00+0200\n2022-09-21T06:00:00+0200'
+        document.cookie = 'cities=Braunschweig Hbf\nWarsaw Centralna\nWien;deps=2022-09-19T06:00:00+0200\n2022-09-21T06:00:00+0200'
     }
     console.log(document.cookie)
     // Daten aus Cookie extrahieren
@@ -12,10 +28,10 @@ function defaultValues(){
     listTimes = document.cookie.split(';')[1]
 
     // Falls es Probleme mit dem Cookie gab
-    if(listCities.length == 0 || listTimes.length == 0){
-        listCities = 'Braunschweig Hbf\nWarsaw Centralna\nWien'
-        listTimes = '2022-09-19T06:00:00+0200\n2022-09-21T06:00:00+0200'
-    }
+    //if(listCities.length == 0 || listTimes.length == 0 || document.cookie = null){
+    listCities = 'Braunschweig\nWarsaw\nWien\nZagreb\nVienna\nRoma\nNice\nBarcelona\nMadrid\nPorto\nLisbon\nMadrid\nToulouse\nParis\nBrussels\nAmsterdam\nBraunschweig'
+    listTimes = '2022-09-19T06:00:00+0200\n2022-09-21T06:00:00+0200\n2022-09-23T06:00:00+0200\n2022-09-25T06:00:00+0200\n2022-09-27T06:00:00+0200\n2022-09-29T06:00:00+0200\n2022-10-01T06:00:00+0200\n2022-10-03T06:00:00+0200\n2022-10-03T06:00:00+0200\n2022-10-05T06:00:00+0200\n2022-10-07T06:00:00+0200\n2022-10-09T06:00:00+0200\n2022-10-11T06:00:00+0200\n2022-10-13T06:00:00+0200\n2022-10-15T06:00:00+0200\n2022-10-19T06:00:00+0200\n2022-10-19T06:00:00+0200'
+    //}
     
     // Füllen der Listen
     document.getElementById('cities').innerHTML = listCities//
@@ -34,9 +50,9 @@ async function fetchStation(name){
 async function calculate() {
     const destinations = document.getElementById('cities').value.split('\n')
     const departures = document.getElementById('departure').value.split('\n')
-    document.cookie = (document.getElementById('cities').value + ';' + document.getElementById('departure').value)
-    document.getElementById('progress').innerHTML = '<img src="https://1.bp.blogspot.com/-rVj7BQjJDrs/YHQV88_S7UI/AAAAAAAAD9o/eZqJQ7RNlY4h366-SfnuAuQHfOCV0S1wACLcBGAsYHQ/s640/Sequence%2B02.gif" style="height: 50px;"/>'
-    //console.log(destinations)
+    document.getElementById('progress').innerHTML = 'Berechnung läuft...'
+    //document.cookie = (document.getElementById('cities').value + ';' + document.getElementById('departure').value)
+    //document.getElementById('progress').innerHTML = '<img src="https://1.bp.blogspot.com/-rVj7BQjJDrs/YHQV88_S7UI/AAAAAAAAD9o/eZqJQ7RNlY4h366-SfnuAuQHfOCV0S1wACLcBGAsYHQ/s640/Sequence%2B02.gif" style="height: 50px;"/>'
     let ids = []
     let route = '' //[]
     for(let i=0; i<destinations.length; i++){
@@ -47,8 +63,8 @@ async function calculate() {
             let traveltimehour = 0;
             let traveltimeminute = 0;
             let route2;
-            
-            let response = await window.interrail.journeys(ids[ids.length-2],ids[ids.length-1], { when: new Date(departures[departures.length - 2])}).then(data => content = data[0]['legs'])
+            document.getElementById('progress').innerHTML = ('Berechne Route ' + destinations[i-1] + ' nach ' + destinations[i] + '...')
+            let response = await window.interrail.journeys(ids[ids.length-2],ids[ids.length-1], { when: new Date(departures[i-1])}).then(data => content = data[0]['legs'])
             for(let j = 0; j<content.length; j++){
                 const trainid = content[j]['line']['id']
                 const trainname = content[j]['line']['name']
@@ -85,7 +101,11 @@ async function calculate() {
                 const fahrzeit = (deltahour + ':' + deltaminute)
 
                 //console.log('Days:'+deltaday+'Hours:'+deltahour+'Minutes:'+deltaminute)
-                route2 += ('<td><h3>Zug: ' + trainname + '</h3>Von ' + citydeparture + '<br>Abfahrt am ' + timedeparture[0] + ' um ' + timedeparture[1].slice(0,5) + '<br>Nach ' + cityarrival + '<br>Ankunft am ' + timearrival[0] + ' um ' + timearrival[1].slice(0,5) + '<br>Fahrzeit: ' + fahrzeit + '</td>')
+                route2 += ('<td><h3>' + trainname + '</h3>Von ' + citydeparture + '<br>Abfahrt am ' + timedeparture[0] + ' um ' + timedeparture[1].slice(0,5) + '<br>Nach ' + cityarrival + '<br>Ankunft am ' + timearrival[0] + ' um ' + timearrival[1].slice(0,5) + '<br>Fahrzeit: ' + fahrzeit + '</td>')
+            }
+            while(traveltimeminute >= 60){
+                traveltimehour += 1
+                traveltimeminute -= 60
             }
             route1 = ('<tr style="background-color: #ededed;"><td><b>#' + i + '</b><br>Fahrzeit: <br>' + traveltimehour + ':' + traveltimeminute + '</td>')
             //route += ('<td>' + content[0] + '</td><td>' + content[1]['line']['id'] + '</td><td>' + content[2]['line']['id'])
